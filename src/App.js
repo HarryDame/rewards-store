bd1ab1ac65cc4b561cdcc6c2911b8aa36db0ff8f
Harry Dame <enriquedabul@gmail.com>
2021-05-25 23:59:52 -0300

    Styles yes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Main from "./components/main/Main";
+import "./styles/styles.css";
 function App() {
   return (
     <div>
