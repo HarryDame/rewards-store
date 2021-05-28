2a4da22e212fa69de2a5f726a0a35d8c133a192e
Harry Dame <enriquedabul@gmail.com>
2021-05-27 23:51:09 -0300

    alredy had Success, ahve to finish AddModal and Error

---
+++ src/components/main/Success.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SuccessModal = ({isSuccess}) => {
+	return(
+		<div className={`success-ctn ${isSuccess ? "success" : "fail"}`} id="success-modal">
+			<h3 className="success-title">{isSuccess ? "Successfully redeemed!" : "Failed to redeem"}</h3>
+			<i className={`fas ${isSuccess ? "fa-check-circle" : "fa-times"} success-icon`}></i>
+		</div>
+	)
+}
+
+export default SuccessModal;
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,66 +1,66 @@
 class UserService {
     static async getUserInfo() {
-            const headers = {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFkOWI2NDliNzc4MTAwMjA5YzVhYTQiLCJpYXQiOjE2MjE5OTAyNDR9.5YW-aBwRpJZS88xYCvlvTv1Ri5x_LphIw7a5NvcC9GA"
-            }
-
-            // const headers = { headers: this.headers }
-            const url = "https://coding-challenge-api.aerolab.co/user/me";
-
-            try {
-                let resp = await fetch(url, { headers })
-
-                if (resp.status !== 200) {
-                    throw new Error(resp);
-                }
-
-                resp = await resp.json();
-                return resp;
-
-            } catch (e) {
-                return false;
-            }
+        const headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFkOWI2NDliNzc4MTAwMjA5YzVhYTQiLCJpYXQiOjE2MjE5OTAyNDR9.5YW-aBwRpJZS88xYCvlvTv1Ri5x_LphIw7a5NvcC9GA"
         }
-        // static async addCoins(amount) {
 
-    //     const params = {
-    //         body: JSON.stringify({ amount: amount, }),
-    //         method: "post",
-    //         mode: "cors",
-    //         headers: this.headers
-    //     }
+        // const headers = { headers: this.headers }
+        const url = "https://coding-challenge-api.aerolab.co/user/me";
 
-    //     try {
-    //         const resp = await fetch(`https://coding-challenge-api.aerolab.co/user/points`, params);
+        try {
+            let resp = await fetch(url, { headers })
 
-    //         if (resp.status !== 200) {
-    //             throw new Error(resp);
-    //         }
+            if (resp.status !== 200) {
+                throw new Error(resp);
+            }
 
-    //         const data = await resp.json();
-    //         return data;
+            resp = await resp.json();
+            return resp;
 
-    //     } catch (e) {
-    //         return false;
-    //     }
-    // }
-    // static async getHistory() {
+        } catch (e) {
+            return false;
+        }
+    }
+    static async addCoins(amount) {
 
-    //     const headers = { headers: this.headers }
+        const params = {
+            body: JSON.stringify({ amount: amount, }),
+            method: "post",
+            mode: "cors",
+            headers: this.headers
+        }
 
-    //     try {
-    //         const resp = await fetch(`https://coding-challenge-api.aerolab.co/user/history`, headers);
+        try {
+            const resp = await fetch(`https://coding-challenge-api.aerolab.co/user/points`, params);
 
-    //         if (resp.status !== 200) {
-    //             throw new Error("Error");
-    //         }
-    //         const data = await resp.json();
-    //         return data;
-    //     } catch (e) {
-    //         return false;
-    //     }
-    // }
+            if (resp.status !== 200) {
+                throw new Error(resp);
+            }
+
+            const data = await resp.json();
+            return data;
+
+        } catch (e) {
+            return false;
+        }
+    }
+    static async getHistory() {
+
+        const headers = { headers: this.headers }
+
+        try {
+            const resp = await fetch(`https://coding-challenge-api.aerolab.co/user/history`, headers);
+
+            if (resp.status !== 200) {
+                throw new Error("Error");
+            }
+            const data = await resp.json();
+            return data;
+        } catch (e) {
+            return false;
+        }
+    }
 }
 export default UserService;
